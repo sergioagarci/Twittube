564d51c789e21269fccd607c3cc7b7215e0dd61a
NoelDiazMesa <noeldiaz83@gmail.com>
2013-04-22 13:03:35 +0100

    Creando el proyecto

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Twittube::Application.config.secret_token = '52480a685f0664b535b1a5f4c6e8d979d2851b727b5a9efb10fc6d7acdf220105750abe1a11b2c255780fa3ebe19951c0a1838d81fb585251d699c5eccabfe06'
