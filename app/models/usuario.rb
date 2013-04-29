48d2a5de4854af1576fcdced12b40f5b27d18337
NoelDiazMesa <noeldiaz83@gmail.com>
2013-04-29 12:27:23 +0100

    Terminada primera version de test de usuarios.

--- app/models/usuario.rb
+++ app/models/usuario.rb
@@ -4,9 +4,9 @@ class Usuario < ActiveRecord::Base
 
    email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
-  validates :username, :presence => true,
-  					   :length   => { :maximum => 50 }
-  validates :email,   :presence => true,
-     				  :format  => { :with => email_regex },
-     				  :uniqueness => { :case_sensitive => false }
+  validates :username, :presence   => true,
+  					   :length     => { :maximum => 50 }
+  validates :email,    :presence   => true,
+     				   :format     => { :with => email_regex },
+     				   :uniqueness => { :case_sensitive => false }
 end
