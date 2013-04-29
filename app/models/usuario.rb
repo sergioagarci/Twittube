4a1b8affb56dfaada07a22fbd2871382761feccf
NoelDiazMesa <noeldiaz83@gmail.com>
2013-04-29 07:10:57 +0100

    Prueba 4

--- app/models/usuario.rb
+++ app/models/usuario.rb
@@ -1,4 +1,4 @@
 class Usuario < ActiveRecord::Base
   authenticates_with_sorcery!
-  validates_confirmation_of :password, message: " Ambos campos deben coincidir", if: :password
+  validates_confirmation_of :password, message: " Ambos campos deben coincidir ", if: :password
 end
