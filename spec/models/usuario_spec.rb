972df32ee8701050c5a5bccb30e72a5328422feb
NoelDiazMesa <noeldiaz83@gmail.com>
2013-04-29 07:19:28 +0100

    Prueba 4 validando modelo superada

--- spec/models/usuario_spec.rb
+++ spec/models/usuario_spec.rb
@@ -1,5 +1,17 @@
 require 'spec_helper'
 
 describe Usuario do
- #  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @attr = { :username => "Example User", :email => "user@example.com" }
+  end
+
+  it "should create a new instance given valid attributes" do
+    Usuario.create!(@attr)
+  end
+
+  it "should require a name" do
+    no_name_user = Usuario.new(@attr.merge(:username => ""))
+    no_name_user.should_not be_valid
+  end
+
 end
