c14107836c9a66da92fe8217c64b47bb835ca1e4
NoelDiazMesa <noeldiaz83@gmail.com>
2013-05-07 11:25:41 +0100

    Actualizando rama 2

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Twittube::Application.routes.draw do
 
   root :to => 'usuarios_sessions#new'  
-  
+
+
   match '/home',    to: 'static_pages#home'
   match '/help',    to: 'static_pages#help'
   match '/about',   to: 'static_pages#about'
