2778fbd10524d166dfac5ecee1eac9b154fe0687
NoelDiazMesa <noeldiaz83@gmail.com>
2013-05-13 12:56:26 +0100

    limpiando rama

--- app/helpers/usuarios_helper.rb
+++
@@ -1,8 +0,0 @@
-module UsuariosHelper
-
-   	def gravatar_for(user, options = { :size => 50 })
-    	gravatar_image_tag(user.email.downcase, :alt => h(user.username),
-                                            	:class => 'gravatar',
-                                            	:gravatar => options)
-   	end
-end
--- spec/factories.rb
+++
@@ -1,7 +0,0 @@
-# By using the symbol ':user', we get Factory Girl to simulate the User model.
-Factory.define :usuario do |user|
-  user.username              "Noel Diaz"
-  user.email                 "noeldiaz@example.com"
-  user.password              "password"
-  user.password_confirmation "password"
-end
